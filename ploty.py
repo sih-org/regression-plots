b373b7e62f07ebe52f5ad75e77d76870865666c7
venkattj <venkattejavasa08@gmail.com>
2019-02-27 17:52:42 +0530

    :q:new file:   clothdata.csv

--- ploty.py
+++ ploty.py
@@ -1,26 +1,35 @@
 import operator
-
+import flask
 import numpy as np 
 import matplotlib.pyplot as plt 
-import pandas as pd 
-
+import pandas as pd
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+#server=flassk.Flask(__name__)
+#app=dash.Dash(__name__,server=server)
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import PolynomialFeatures
-datas = pd.read_csv('xysample.csv') 
+data = pd.read_csv('cloth$data.csv') 
 
 np.random.seed(0)
 #x = 2 - 3 * np.random.normal(0, 1, 20)
 #y = x - 2 * (x ** 2) + 0.5 * (x ** 3) + np.random.normal(-3, 3, 20)
+print(data)
+x=data['Total plastic waste '][:].values
+y = data['Residential & commercial (tonnes)'][:].values
+x1=data['Total plastic waste '][:].values
 
-x = datas.iloc[:, 0].values 
-y = datas.iloc[:, 1].values
-x1=[[10],[12],[200]]
 # transforming the data to include another axis
 x = x[:, np.newaxis]
 y = y[:, np.newaxis]
+x1 = x1[:, np.newaxis]
 
-polynomial_features= PolynomialFeatures(degree=2)
+
+polynomial_features= PolynomialFeatures(degree=1)
 x_poly = polynomial_features.fit_transform(x)
 
 model = LinearRegression()
@@ -31,12 +40,33 @@ rmse = np.sqrt(mean_squared_error(y,y_poly_pred))
 r2 = r2_score(y,y_poly_pred)
 print(rmse)
 print(r2)
-#model.predict(polynomial_features.fit_transform(0.5)) 
-print(model.predict(polynomial_features.fit_transform(x1)))
-plt.scatter(x, y, s=200)
+
+
+#model.predict(polynomial_features.fit_transform(0.5))
+k=model.predict(polynomial_features.fit_transform(x1))
+for i in k:
+        for j in i:
+            value=str(j)
+            value2=value.replace(',', '.')
+            print(value2)
+print(model.predict(polynomial_features.fit_transform(x)))
+plt.scatter(x, y, s=20)
 # sort the values of x before line plot
 sort_axis = operator.itemgetter(0)
 sorted_zip = sorted(zip(x,y_poly_pred), key=sort_axis)
 x, y_poly_pred = zip(*sorted_zip)
-plt.plot(x, y_poly_pred, color='m')
+k=plt.plot(x, y_poly_pred, color='m')
 plt.show()
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1(children='Dash Tutorials'),
+    dcc.Graph()
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    dcc.Graph(
+        id='example',
+        figure=k
+    )
+
--- xyplot.py
+++ xyplot.py
@@ -3,46 +3,36 @@ import numpy as np
 from io import StringIO
 import plotly.plotly as py
 import plotly.graph_objs as go
-import plotly
-import numpy
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from sklearn.linear_model import LinearRegression 
-frame=pd.read_csv("xysample.csv",dtype={0:'int',1:'int'})
+frame=pd.read_csv("clothdata.csv")
 
 x1=[]
 y1=[]
-
-def estimate_coef(x, y): 
-    # number of observations/points 
-    n = np.size(x) 
-  
-    # mean of x and y vector 
-    m_x, m_y = np.mean(x), np.mean(y) 
-  
-    # calculating cross-deviation and deviation about x 
-    SS_xy = np.sum(y*x) - n*m_y*m_x 
-    SS_xx = np.sum(x*x) - n*m_x*m_x 
-  
-    # calculating regression coefficients 
-    b_1 = SS_xy / SS_xx 
-    b_0 = m_y - b_1*m_x 
-  
-    return(b_0, b_1) 
-for i in frame['x']:
-    x1.append(i)
-for i in frame['y']:
-    y1.append(i)
+ 
+for i in frame['Year']:
+    x1.append(int(i))
+for i in frame['Residential & commercial (tonnes)']:
+    y1.append(float(i))
 x2=np.array(x1)
 y2=np.array(y1)
-b = estimate_coef(x2, y2) 
-print(b[0],b[1])
+N=100
+random_x = np.random.randn(N)
+random_y = np.random.randn(N)
+
+# Create a trace
+trace = go.Scatter(
+    x = x2,
+    y = y2,
+    mode = 'markers+lines'
+)
+
+data = [trace]
+
+# Plot and embed in ipython notebook!
 
 
 # plot
-trace = go.catter(
-    x=x1,
-    y=y1,
-    mode='markers')
 data=[trace]
 plot(data)
 
